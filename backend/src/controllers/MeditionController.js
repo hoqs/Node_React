50f0c800bcd025267f2e64dd12f44ec435d15084
Henrique Oliveira Quevedo <henrique.quevedo@MLTBF.LOCAL>
2021-02-02 13:44:51 -0300

    mudanças

--- backend/src/controllers/MeditionController.js
+++ backend/src/controllers/MeditionController.js
@@ -1,40 +1,44 @@
 const connection = require("../database/connection");
 
-module.exports={
-    
-    async index (request, response){
-        const order_id = request.header.authorization;
-        const showMeditions = await connection("meditions").where("order_id", order_id).select("*");
-        response.json(showMeditions);
-    },
+module.exports = {
+  async index(request, response) {
+    const order_id = request.header.authorization;
+    const showMeditions = await connection("meditions")
+      .where("order_id", order_id)
+      .select("*");
+    response.json(showMeditions);
+  },
 
-    async create (request, response){
-        const order_id = request.header.authorization;
-        const user_id = request.header.authorization;
-        const { data, hour, medition, temperature, bomb_state } = request.body;
+  async create(request, response) {
+    const order_id = request.header.validation;
+    const user_id = request.header.authorization;
+    const { data, hour, medition, temperature, bomb_state } = request.body;
 
-        const newMedition = await connection("meditions")
-        .insert(
-            order_id,
-            data, 
-            hour, 
-            medition, 
-            temperature, 
-            bomb_state, 
-            user_id);
-            response.json(newMedition);
-    },
+    const newMedition = await connection("meditions").insert(
+      order_id,
+      data,
+      hour,
+      medition,
+      temperature,
+      bomb_state,
+      user_id
+    );
+    response.json(newMedition);
+  },
 
-    async delete (request, response){
-        const order_id = request.header.authorization;
-        const id = request.params;
+  async delete(request, response) {
+    const order_id = request.header.authorization;
+    const id = request.params;
 
-        const orderMedition = await connection("meditions").where("id", id).select("order_id").first();
+    const orderMedition = await connection("meditions")
+      .where("id", id)
+      .select("order_id")
+      .first();
 
-        if(orderMedition.order_id =! order_id){
-            return response.status(401).json({ error: 'Operation not permitted.'});
-        }
-
-        await connection("meditions").where("id", id).delete();
+    if ((orderMedition.order_id = !order_id)) {
+      return response.status(401).json({ error: "Operation not permitted." });
     }
-}
+
+    await connection("meditions").where("id", id).delete();
+  },
+};
